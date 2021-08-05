0571bb346dd5f6f8f5244655c5eb6f7a20178b11
wlgemini <wangluguang@live.com>
2021-08-05 10:47:45 +0800

    .

---
+++ Sources/BinaryCodable/Encoder/BinaryEncoder.swift
@@ -0,0 +1,29 @@
+//
+//  BinaryEncoder.swift
+//  
+
+
+public final class BinaryEncoder: Encoder {
+    
+    public var codingPath: [CodingKey] = []
+    
+    public var userInfo: [CodingUserInfoKey : Any]
+    
+    public func container<Key>(keyedBy type: Key.Type) -> KeyedEncodingContainer<Key> where Key : CodingKey {
+        
+    }
+    
+    public func unkeyedContainer() -> UnkeyedEncodingContainer {
+        
+    }
+    
+    public func singleValueContainer() -> SingleValueEncodingContainer {
+        
+    }
+    
+    public init() { }
+
+    public func encode<T>(_ value: T) throws -> [UInt8] where T : Encodable {
+        
+    }
+}
